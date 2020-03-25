744371e26ef319d5e162b903dbea438150045b15
c19y9x <cyx1213546989@gmail.com>
2020-03-25 16:06:15 +0800

    大文本存储取出

---
+++ src/main/java/web/Servlet/NewServlet.java
@@ -0,0 +1,35 @@
+package web.Servlet;
+
+import domain.Lanmu;
+import domain.News;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import util.JDBCUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/newServlet")
+public class NewServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+
+        JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
+        String sql = "SELECT * FROM news";
+        List<News> newss = template.query(sql, new BeanPropertyRowMapper<News>(News.class));
+        System.out.println(newss);
+        //3.将PageBean存入request
+        request.setAttribute("newss",newss);
+        //4.转发到list.jsp
+        request.getRequestDispatcher("/erji.jsp").forward(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
--- src/main/webapp/erji.jsp
+++ src/main/webapp/erji.jsp
@@ -6,6 +6,8 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <meta charset="utf-8">
@@ -203,7 +205,7 @@
                 <table cellspacing="" cellpadding="" border="0">
                     <tbody>
                     <tr>
-                        <td align="center"><span class="titlestyle134386">尚无资料</span></td>
+                        <td align="center"><span class="titlestyle134386">${newss[0].content}</span></td>
                     </tr>
                     </tbody>
                 </table>
